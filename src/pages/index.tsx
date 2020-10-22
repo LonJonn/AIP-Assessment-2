802c04137ee0b07f51852e88d1e855b55ddfb106
kevinguyen76 <kevin_nguyen_76@hotmail.com>
2020-10-22 11:47:19 +1100

    refactor: asethetic changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,33 @@
 import React from "react";
 import Link from "next/link";
-import { Button, useColorMode } from "@chakra-ui/core";
+import { Button, useColorMode, Heading } from "@chakra-ui/core";
+import { Container } from "next/app";
 
 const Home: React.FC = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <div>
-      <main>Main content here</main>
+      <Container maxW="5xl" centerContent>
+        <Heading
+          as="h1"
+          size="2xl"
+          m="10"
+          fontSize="200px"
+          textAlign="center"
+          paddingTop="125px"
+          paddingBottom="0"
+          letterSpacing="15px"
+        >
+          Pinki
+        </Heading>
 
-      <p>
+        <Heading as="h4" size="xl" fontSize="20px" textAlign="center">
+          I Owe... Who??
+        </Heading>
+      </Container>
+
+      {/* <p>
         <Button color="orange.300" variant="outline" onClick={toggleColorMode}>
           mode: {colorMode}
         </Button>
@@ -22,7 +40,7 @@ const Home: React.FC = () => {
         <a>Test</a>
       </Link>
 
-      <footer>Footer content here</footer>
+      <footer>Footer content here</footer> */}
     </div>
   );
 };
