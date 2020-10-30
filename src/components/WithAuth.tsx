d4af0426a6bca0275655d6dd278d11a2192db7b4
LonJonn <leon.salsiccia@gmail.com>
2020-10-30 12:30:17 +1100

    refactor: auth guard toast

--- src/components/WithAuth.tsx
+++ src/components/WithAuth.tsx
@@ -1,3 +1,4 @@
+import { toast, useToast } from "@chakra-ui/core";
 import { useAuth } from "hooks/useAuth";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
@@ -6,9 +7,17 @@ const withAuth = (WrappedComponent) => {
   const AuthGuard = (props) => {
     const { loading, accessToken } = useAuth();
     const router = useRouter();
+    const toast = useToast();
 
     useEffect(() => {
-      if (!loading && !accessToken) router.push("/login");
+      if (!loading && !accessToken) {
+        toast({
+          status: "warning",
+          title: "You must be logged in to view to page!",
+        });
+
+        router.push("/login");
+      }
     }, [loading, accessToken, router]);
 
     // if there's a loggedInUser, show the wrapped page, otherwise show a loading indicator
